c7e0db879386132cd6fd2df453a5ecf49f862f39
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-26 11:44:06 -0300

    Criação do README e adição do .gitignore, além de desativação de parte do código principal

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto OOP - FIAP
+Repositório didático para estudo de programação orientada à objetos (do inglês *OOP*).
+
+# Objeto selecionado
+Para essa atividade, foi escolhido uma **garrafa** como objeto
+
+## Como usar
+A classe conta com atributos como tamanho, capacidade, líquido e se está aberta. Além disso, possui um método para abri-la ou fecha-la, também como para beber. Os dois métodos iniciais alteram o estado da variável booleana, enquanto o final apenas exibe uma mensagem no terminal baseada no estado da garrafa.
--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -4,19 +4,19 @@ import br.com.fiapride.model.*;
 
 public class SistemaPrincipal {
 	public static void main(String args[]) {
-		Passageiro passageiro1 = new Passageiro();
-		
-		passageiro1.nome = "Ana Silva";
-		passageiro1.saldo = 50.0;
-		
-		Passageiro passageiro2 = new Passageiro();
-				
-		passageiro2.nome = "Carlos Souza";
-		passageiro2.saldo = 12.50;
-		
-		System.out.println("--- Sistema FiapRide ---");
-		System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
-		System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
+//		Passageiro passageiro1 = new Passageiro();
+//		
+//		passageiro1.nome = "Ana Silva";
+//		passageiro1.saldo = 50.0;
+//		
+//		Passageiro passageiro2 = new Passageiro();
+//				
+//		passageiro2.nome = "Carlos Souza";
+//		passageiro2.saldo = 12.50;
+//		
+//		System.out.println("--- Sistema FiapRide ---");
+//		System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
+//		System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
 		
 		// Teste paralelo (para não criar em outro lugar)
 		Garrafa garrafa1 = new Garrafa();
