723942f69fd55ba348353c807479ac2c3dac54d2
Gustavo Hackime Costa <gustavohackime@gmail.com>
2026-03-27 09:31:29 -0300

    Adicionada classe líquido para abstrair na classe Garrafa

---
+++ src/br/com/fiapride/model/Liquido.java
@@ -0,0 +1,13 @@
+package br.com.fiapride.model;
+
+public class Liquido {
+	final private String nome;
+	
+	public Liquido(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+}
